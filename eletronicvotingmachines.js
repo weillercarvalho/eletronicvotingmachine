d041d3810ed817a8098b799c0bde1c02e32202d4
Weiller Carvalho <engweiller@hotmail.com>
2022-05-11 12:26:58 -0300

    javascriptfinishing

---
+++ eletronicvotingmachines.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'Image01.png', legenda: 'Vereador'}
+                ]
+
+
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'Image02.jfif', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:99,
+                nome:'Gato Rosa',
+                partido: 'ABC',
+                vice: 'Gato',
+                fotos: [
+                    {url:'Image01.png', legenda:'Prefeito'},
+                    {url:'Image02.jfif', legenda:'Vice-Prefeito', small:true},
+
+                ]
+            },
+            {
+                numero:84,
+                nome:'Gato Preto',
+                partido: 'QWERT',
+                vice: 'Gato Marrom',
+                fotos: [
+                    {url:'Image01.png', legenda:'Prefeito'},
+                    {url:'Image02.jfif', legenda:'Vice-Prefeito', small:true},
+                ]
+            }
+        ]   
+    }
+
+]
